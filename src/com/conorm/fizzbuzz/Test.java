240160ab5d83d7703819222393b5969fa5062f8d
Conor Mendenhall <conor.mendenhall@gmail.com>
2015-08-26 14:19:15 -0400

    refactored test setup

--- src/com/conorm/fizzbuzz/Test.java
+++ src/com/conorm/fizzbuzz/Test.java
@@ -2,16 +2,19 @@ package com.conorm.fizzbuzz;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+
 public class Test {
 
-	@org.junit.Test
-	public void shouldBeFizzBuzzClass() {
-		FizzBuzz fb = new FizzBuzz();
+	private FizzBuzz fb;
+
+	@Before
+	public void setUp() {
+		fb = new FizzBuzz();
 	}	
 
 	@org.junit.Test
 	public void shouldConvert() {
-		FizzBuzz fb = new FizzBuzz();
 		fb.convert(1);
 	}
 }
