3093752e04d63cb0dfdcd54e53617cffe7122102
Conor Mendenhall <conor.mendenhall@gmail.com>
2015-08-26 14:06:00 -0400

    JUnit test created

---
+++ src/com/conorm/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,5 @@
+package com.conorm.fizzbuzz;
+
+public class FizzBuzz {
+
+}
---
+++ src/com/conorm/fizzbuzz/Test.java
@@ -0,0 +1,12 @@
+package com.conorm.fizzbuzz;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void shouldBeFizzBuzzClass() {
+		FizzBuzz fb = new FizzBuzz();
+	}	
+
+}
