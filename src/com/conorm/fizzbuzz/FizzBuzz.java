d365aa3795927259b29c9e117ecec42099a0724e
Conor Mendenhall <conor.mendenhall@gmail.com>
2015-08-26 14:39:55 -0400

    refactor: extracted method isMultipleOf3

--- src/com/conorm/fizzbuzz/FizzBuzz.java
+++ src/com/conorm/fizzbuzz/FizzBuzz.java
@@ -3,11 +3,13 @@ package com.conorm.fizzbuzz;
 public class FizzBuzz {
 
 	public String convert(int number) {
-		
-		if (number % 3 == 0) return "Fizz";
+		if (isMultipleOf3(number)) return "Fizz";
 		else if (number == 5) return "Buzz";
 		return Integer.toString(number);
-	
+	}
+
+	private boolean isMultipleOf3(int number) {
+		return number % 3 == 0;
 	}
 
 }
